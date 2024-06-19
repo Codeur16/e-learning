7a5f808f51756a07dc2f45edf9e2ba64b4d280fe
Codeur16 <charlesnjiosseu@gmail.com>
2024-06-19 16:58:29 +0100

    dateNaissance

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -186,9 +186,7 @@ SujetTable.belongsTo(DomaineTable, {
 async function initDB() {
   console.log("Initialisation des tables de la base de données");
   try {
-    await sequelize
-      .sync
-      ({alter:true});
+    await sequelize.sync({ force: true });
     console.log("Tables have been created");
   } catch (error) {
     console.error("Unable to create tables:", error);
