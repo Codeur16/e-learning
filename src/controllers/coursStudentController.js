61daa09cabe0e76c2c7845cd3a48950e4497b75f
Codeur16 <charlesnjiosseu@gmail.com>
2024-06-26 11:41:00 +0100

    new table suivicours

--- src/controllers/coursStudentController.js
+++ src/controllers/coursStudentController.js
@@ -1,41 +1,101 @@
-const {  StudentTable, CoursTable  } = require("../db/sequelize");
+const {
+  StudentTable,
+  CoursTable,
+  SuivicoursTable,
+  ChapitreTable,
+} = require("../db/sequelize");
 
 const sendResponse = (res, statusCode, message, data = null) => {
   res.status(statusCode).json({ message, data });
 };
 
+// const addStudentToCours = async (req, res) => {
+//    const studentId = req.params.studentId;
+//    const coursId = req.params.coursId;
+
+//   try {
+//     const student = await StudentTable.findByPk(studentId);
+//     if (!student) {
+//       return sendResponse(res, 201, "Étudiant non trouvé");
+//     }
+
+//     const cours = await CoursTable.findByPk(coursId);
+//     if (!cours) {
+//       return sendResponse(res, 201, "Cours non trouvé");
+//     }
+
+//     await student.addCours(cours);
+//     sendResponse(res, 200, "Étudiant ajouté au cours avec succès", cours);
+//   } catch (error) {
+//     console.error("Erreur lors de l'ajout de l'étudiant au cours :", error);
+//     sendResponse(
+//       res,
+//       500,
+//       "Erreur lors de l'ajout de l'étudiant au cours",
+//       error
+//     );
+//   }
+// };
+
+/*
+// ajouter un student a un cours
+*/
+
+//const { StudentTable, CoursTable, SuivicoursTable, ChapitreTable } = require('../models');
+
 const addStudentToCours = async (req, res) => {
-   const studentId = req.params.studentId;
-   const coursId = req.params.coursId; 
+  const studentId = req.params.studentId;
+  const coursId = req.params.coursId;
 
   try {
     const student = await StudentTable.findByPk(studentId);
     if (!student) {
-      return sendResponse(res, 201, "Étudiant non trouvé");
+      return res.status(404).json({ message: "Étudiant non trouvé" });
     }
 
-    const cours = await CoursTable.findByPk(coursId);
+    const cours = await CoursTable.findByPk(coursId, {
+      include: {
+        model: ChapitreTable,
+        as: "chapitres",
+        order: [["chapitreId", "ASC"]], // Assurez-vous que les chapitres sont triés par leur ID
+      },
+    });
+
     if (!cours) {
-      return sendResponse(res, 201, "Cours non trouvé");
+      return res.status(404).json({ message: "Cours non trouvé" });
     }
-
     await student.addCours(cours);
-    sendResponse(res, 200, "Étudiant ajouté au cours avec succès", cours);
+    let msg = "l'etudiant a ete inscrit au cours , ";
+
+    // Trouver le premier chapitre du cours ou définir sur null si le cours n'a pas de chapitre
+    const firstChapitre =
+      cours.chapitres.length > 0 ? cours.chapitres[0] : null;
+    const firstChapitreId = firstChapitre ? firstChapitre.chapitreId : null;
+
+    // Créer une nouvelle entrée dans SuivicoursTable
+    await SuivicoursTable.create({
+      studentId: studentId,
+      coursId: coursId,
+      currentChapitreId: firstChapitreId,
+      chapitresCompletes: JSON.stringify([]), // Initialiser comme une chaîne JSON vide
+    });
+    msg = msg + "et Etudiant ajouté au suivi des cours avec succès";
+
+    res.status(200).json({ message: msg });
   } catch (error) {
     console.error("Erreur lors de l'ajout de l'étudiant au cours :", error);
-    sendResponse(
-      res,
-      500,
-      "Erreur lors de l'ajout de l'étudiant au cours",
-      error
-    );
+    res
+      .status(500)
+      .json({
+        message: "Erreur lors de l'ajout de l'étudiant au cours",
+        error,
+      });
   }
 };
 
-
 const removeStudentFromCours = async (req, res) => {
   const studentId = req.params.studentId;
-  const coursId = req.params.coursId; 
+  const coursId = req.params.coursId;
 
   try {
     const student = await StudentTable.findByPk(studentId);
@@ -64,9 +124,8 @@ const removeStudentFromCours = async (req, res) => {
   }
 };
 
-
 const getStudentsInCours = async (req, res) => {
-  const coursId = req.params.coursId; 
+  const coursId = req.params.coursId;
 
   try {
     const cours = await CoursTable.findByPk(coursId, {
@@ -90,7 +149,6 @@ const getStudentsInCours = async (req, res) => {
     );
   }
 };
-
 
 const getCoursForStudent = async (req, res) => {
   const { studentId } = req.params;
